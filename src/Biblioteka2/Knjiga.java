162a1e60522281f66b94cc975589f5a51d1fc5a9
Zuhdija Delic <zuka_delic@hotmail.com>
2019-05-07 12:08:56 +0200

    Prvi commit

---
+++ src/Biblioteka2/Knjiga.java
@@ -0,0 +1,69 @@
+package Biblioteka2;
+
+import java.util.ArrayList;
+
+public class Knjiga {
+	
+	public int brojKnjige;
+	public String imeKnjige;
+	public boolean statusKnjigeDaLiJeKnjigaIzdataIliNe;
+	
+	public int getBrojKnjige() {
+		return brojKnjige;
+	}
+
+	public void setBrojKnjige(int brojKnjige) {
+		this.brojKnjige = brojKnjige;
+	}
+
+	public String getImeKnjige() {
+		return imeKnjige;
+	}
+
+	public void setImeKnjige(String imeKnjige) {
+		this.imeKnjige = imeKnjige;
+	}
+
+	public boolean isStatusKnjigeDaLiJeKnjigaIzdataIliNe() {
+		return statusKnjigeDaLiJeKnjigaIzdataIliNe;
+	}
+
+	public void setStatusKnjigeDaLiJeKnjigaIzdataIliNe(
+			boolean statusKnjigeDaLiJeKnjigaIzdataIliNe) {
+		this.statusKnjigeDaLiJeKnjigaIzdataIliNe = statusKnjigeDaLiJeKnjigaIzdataIliNe;
+	}
+
+	public static ArrayList<Knjiga> knjigaArrayLista = new ArrayList<>();
+	
+	public Knjiga(){
+		
+	}
+	
+	public Knjiga(int brojKnjige, String imeKnjige, boolean statusKnjigeDaLiJeKnigaIzdataIliNe){
+		
+		this.brojKnjige = brojKnjige;
+		this.imeKnjige = imeKnjige;
+		this.statusKnjigeDaLiJeKnjigaIzdataIliNe = statusKnjigeDaLiJeKnigaIzdataIliNe;
+		
+		knjigaArrayLista.add(this);
+		
+	}
+	
+	public static boolean daLiPostojiVecKnjigaSaIstimIDom (int brojKnjige){
+		for (int i = 0; i < knjigaArrayLista.size(); i++){
+			if (knjigaArrayLista.get(i).brojKnjige == brojKnjige){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean daLiJeUneseniBrojKnjigeNegativan (int brojKnjige){
+		if (brojKnjige < 0){
+			return true;
+			
+		}
+		return false;
+	}
+	
+}
---
+++ src/Biblioteka2/Zapisnik.java
@@ -0,0 +1,45 @@
+package Biblioteka2;
+
+public class Zapisnik {
+	
+	
+		public static String zapisnik = "";
+
+		public void kreacijaRacuna(int brojRacuna, String musterija) {
+
+			zapisnik += "Kreacija racuna: " + brojRacuna + " " + musterija;
+
+		}
+
+		public void kreacijaKnjige(int brojKnjige, String imeKnjige) {
+
+			zapisnik += "Kreacija knjige: " + brojKnjige + " " + imeKnjige;
+
+		}
+
+		public void podizanjeKnjige(int brojKnjige, String imeKnjige,
+				int brojRacuna, String musterija, String datumPodizanja) {
+
+			zapisnik += "--- Podizanje knjige --- " + brojKnjige + imeKnjige
+					+ "--- Racun --- " + brojRacuna + musterija
+					+ "--- Datum podizanja ---" + datumPodizanja;
+
+		}
+
+		public void vracanjeKnjige(int brojKnjige, String imeKnjige,
+				int brojRacuna, String musterija, String datumVracanja) {
+
+			zapisnik += "--- Vracanje knjige ---" + brojKnjige + imeKnjige
+					+ "--- Racun ---" + brojKnjige + musterija
+					+ "--- Datum vracanja ---" + datumVracanja;
+
+		}
+
+		public static String ispisi() {
+			return zapisnik;
+		}
+
+	}
+
+
+
